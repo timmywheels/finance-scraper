89eb251a0c777db4208d9933bdc15483640f5e0a
timwheelercom <tim@timwheeler.com>
2019-02-08 08:56:44 -0500

    moved repo

---
+++ src/main/java/Data.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Data {
+
+    private Connection connect(){
+        String url = "jdbc:sqlite:src/main/db/stock_portfolio.db";
+        Connection db = null;
+
+        try {
+            db = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return db;
+
+    }
+
+    public void insert(String symbol, String companyName, String lastPrice, String change, String percentChange) {
+        System.out.println("Saving data...");
+        String sql = "INSERT INTO stocks(symbol, companyName, lastPrice, change, percentChange) VALUES(?,?,?,?,?)";
+        try (Connection db = this.connect();
+             PreparedStatement pstmt = db.prepareStatement(sql)) {
+                pstmt.setString(1, symbol);
+                pstmt.setString(2, companyName);
+                pstmt.setString(3, lastPrice);
+                pstmt.setString(4, change);
+                pstmt.setString(5, percentChange);
+                pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+            String baseUrl = "https://finance.yahoo.com";
+            String loginUrl = "https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F";
+            String username = "tim@timwheeler.com";
+            String password = "mvp71111" ;
+            String portfolioUrl = "https://finance.yahoo.com/portfolio/p_0/view/v1";
+
+            Auth.webClient(loginUrl, username, password, portfolioUrl);
+
+        }
+
+    }
+
