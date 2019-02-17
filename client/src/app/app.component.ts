dae793a599a1741ef5177f46cab30aa2909ed462
Tim Wheeler <tim@timwheeler.com>
2019-02-17 00:17:02 -0500

    frontend displaying api data in console

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import * as Http from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
+import {Resolve} from "@angular/router";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+
+})
+export class AppComponent {
+  title = 'client';
+  private apiUrl = 'http://localhost:4567/api/snapshot/001';
+  data: any = {};
+
+  constructor(private http: HttpClient) {
+      console.log("Hello, API")
+      this.getData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl)
+      .subscribe();
+  }
+}
+
+
--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -1,15 +1,36 @@
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import spark.*;
+
+import java.util.HashMap;
 
 import static spark.Spark.get;
 
 public class Api extends Data {
 
+    private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
+
+    static {
+        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        corsHeaders.put("Access-Control-Allow-Origin", "*");
+        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+        corsHeaders.put("Access-Control-Allow-Credentials", "true");
+    }
+
+    public final static void applyCORSFilter() {
+        Filter filter = new Filter() {
+            @Override
+            public void handle(Request request, Response response) {
+                corsHeaders.forEach((key, value) -> {
+                    response.header(key, value);
+                });
+            }
+        };
+        Spark.after(filter);
+    }
 
     public static String server() {
         String sql = "SELECT timeStamp, symbol, companyName, lastPrice, change, percentChange FROM stocks";
         String json = ResultSetToJson.resultSetToJson(Api.connect(), sql);
+        Api.applyCORSFilter();
         get(new Route("/api/snapshot/:id") {
             @Override
             public Object handle(Request request, Response response) {
